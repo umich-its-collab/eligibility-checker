62328af0d7308c09e67bc3e7bc664d4d70218a6a
Maggie Davidson <jmagdavidson@gmail.com>
2022-08-09 16:37:02 -0400

    Forgot to move test app and secret out

--- tests/mcommunity/test_mcommunity_group.py
+++ tests/mcommunity/test_mcommunity_group.py
@@ -4,8 +4,7 @@ from unittest.mock import patch
 
 from mcommunity.mcommunity_group import MCommunityGroup
 from mcommunity.mcommunity_user import MCommunityUser
-from tests.mcommunity import mcommunity_mocks
-from tests.mcommunity.test_mcommunity_base import test_app, test_secret
+from tests.mcommunity import mcommunity_mocks as mocks
 
 test_group = 'test-group'
 
@@ -16,14 +15,14 @@ class MCommunityGroupTestCase(unittest.TestCase):
     @classmethod
     @patch('mcommunity.mcommunity_group.MCommunityGroup._populate_group_data')
     def setUpClass(cls, magic_mock) -> None:
-        magic_mock.side_effect = mcommunity_mocks.mcomm_group_side_effect
-        cls.group = MCommunityGroup(test_group, test_app, test_secret)
+        magic_mock.side_effect = mocks.mcomm_group_side_effect
+        cls.group = MCommunityGroup(test_group, mocks.test_app, mocks.test_secret)
 
     def test_init_sets_cn(self):
         self.assertEqual(test_group, self.group.cn)
 
     def test_init_sets_raw_group(self):
-        self.assertEqual(mcommunity_mocks.group_mock, self.group.raw_group)
+        self.assertEqual(mocks.group_mock, self.group.raw_group)
         self.assertEqual(list, type(self.group.raw_group))
         self.assertEqual(1, len(self.group.raw_group))  # LDAP should always return a 1-item list for a real group
         self.assertEqual(2, len(self.group.raw_group[0]))  # LDAP should always return a 2-item tuple for a real group
@@ -34,7 +33,7 @@ class MCommunityGroupTestCase(unittest.TestCase):
     @patch('mcommunity.mcommunity_user.MCommunityUser._populate_user_data')
     def test_populate_members_mcomm_users(self, magic_mock):
         print('mcomm users')
-        magic_mock.side_effect = mcommunity_mocks.mcomm_user_side_effect
+        magic_mock.side_effect = mocks.mcomm_user_side_effect
         self.assertEqual([], self.group.members_mcomm_users)  # Before populating, there should not be any
         self.group.populate_members_mcomm_users()
         members = [
@@ -49,9 +48,9 @@ class MCommunityGroupTestCase(unittest.TestCase):
 
     @patch('mcommunity.mcommunity_group.MCommunityGroup._populate_group_data')
     def test_group_not_exists(self, magic_mock):
-        magic_mock.side_effect = mcommunity_mocks.mcomm_group_side_effect
+        magic_mock.side_effect = mocks.mcomm_group_side_effect
         with self.assertRaises(NameError):
-            self.assertEqual(False, MCommunityGroup('fake', test_app, test_secret).exists)
+            self.assertEqual(False, MCommunityGroup('fake', mocks.test_app, mocks.test_secret).exists)
 
 
 if __name__ == '__main__':
