6a8f1c31faa19dcc7d869ab326ad21de00d74c2b
Maggie Davidson <31591389+mfldavidson@users.noreply.github.com>
2022-10-28 14:48:43 -0400

    Bump elg checker version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='eligibility-checker',
-      version='0.6',
+      version='0.7',
       description='Standardized eligibility checking for U-M ITS Collaboration Services',
       url='https://github.com/umich-its-collab/eligibility-checker',
       author='University of Michigan ITS Collaboration Services',
